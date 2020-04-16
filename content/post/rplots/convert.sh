8a93bd421ea1672880e0c0a4bbbc36978774605b
Eoin Travers <eoint02@gmail.com>
2020-04-16 18:54:02 +0100

    New ggplot post

---
+++ content/post/rplots/convert.sh
@@ -0,0 +1,6 @@
+# Rscript -e "rmarkdown::render('index.Rmd')"
+Rscript -e "rmarkdown::render('index.Rmd', rmarkdown::md_document(preserve_yaml=TRUE))"
+perl -0777 -i.original -pe 's/\n```\n*```[A-z]*//igs' index.md  # Join neighbouring code blocks
+perl -0777 -i.original -pe 's/\\\\\n/\\\\\\\\\n/igs' index.md  # Double tex newlines
+rm index.html
+# perl -0777 -i.original -pe 's/a test\nPlease do not/not a test\nBe/igs' alpha.txt
